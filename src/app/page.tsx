b847673a60c2e86276b2c98cbded61718c25e19f
Priyanshu <168187726+Priyanshu0105@users.noreply.github.com>
2026-04-09 02:24:10 +0530

    adding canvas element to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Cursor from "@/components/Cursor";
 import Hero from "@/components/Hero";
 import ScrollSection from "@/components/ScrollSection";
 import History from "@/components/History";
@@ -8,6 +8,7 @@ import Details from "@/components/Details";
 export default function Home() {
   return (
     <main>
+      <Cursor/>
       <Hero />
       <ScrollSection />
       <History />
