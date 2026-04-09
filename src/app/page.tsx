a4166be84fed7ab10bc6ad4948a74907f6b6d294
Priyanshu <168187726+Priyanshu0105@users.noreply.github.com>
2026-04-09 20:23:50 +0530

    adding loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,22 @@ import Hero from "@/components/Hero";
 import ScrollSection from "@/components/ScrollSection";
 import History from "@/components/History";
 import Details from "@/components/Details";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import Loader from "@/components/Loader";
+
 export default function Home() {
   return (
-    <main>
-      <Cursor/>
-      <Analytics />
-      <Hero />
-      <ScrollSection />
-      <History />
-      <Details />
-    </main>
+    <>
+      <Loader />
+
+      <main>
+        <Cursor/>
+        <Analytics />
+        <Hero />
+        <ScrollSection />
+        <History />
+        <Details />
+      </main>
+    </>
   );
 }
