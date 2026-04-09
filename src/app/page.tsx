3a7920f749ba6be8df1096891157fa63bb6a3b6a
Priyanshu <168187726+Priyanshu0105@users.noreply.github.com>
2026-04-09 18:37:41 +0530

    added vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import Hero from "@/components/Hero";
 import ScrollSection from "@/components/ScrollSection";
 import History from "@/components/History";
 import Details from "@/components/Details";
-
+import { Analytics } from "@vercel/analytics/next"
 export default function Home() {
   return (
     <main>
       <Cursor/>
+      <Analytics />
       <Hero />
       <ScrollSection />
       <History />
